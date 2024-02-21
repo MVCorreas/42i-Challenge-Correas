913794ac98e9de9416cbd1e8836d94bb8bb26cec
MVCorreas <correasmv@gmail.com>
2024-02-21 11:42:01 -0300

    twoNumberSum documented

--- README.md
+++ README.md
@@ -1,15 +1,28 @@
+                                                            42i CHALLENGE
+
+Read the following two problems and create a suitable function. For each function, create suitable testing units.
+The alloted time is 72 h.
+You will need to hand in the link to the Github repository.
+
 CHALLENGE TEST 1
 
-# Two Number Sum
+# TWO NUMBER SUM
 
 Given an array of integers, no number in this array is repeated, and an integer representing the target sum, implement a function that find whether there's a pair of numbers in the array that adds up to the target sum. Return the pair in an array. If such pair does not exist, return an empty array.
 Examples:
-const numbers = [2, 7, 11, 15];
-const targetSum = 9;
 
 ### **Sample Input**
-findPairWithSum(numbers, targetSum); // Input: [2, 7]
+
+`const numbers = [2, 7, 11, 15];`
+`const targetSum = 9;`
+
+### **Sample Output**
+
+`findPairWithSum(numbers, targetSum); // Input: [2, 7]`
 ​
+
+### **Sample Input**
+
 `const numbers = [3, 6, 8, 10];`
 `const targetSum = 15;`
 
@@ -19,7 +32,7 @@ findPairWithSum(numbers, targetSum); // Input: [2, 7]
 
 CHALLENGE TEST 2
 
-# Non-Constructible Change
+# NON-CONSTRUCTIBLE CHANGE
 
 Given an array of positive integers representing the values of coins in your possession, write a function that returns the minimum amount of change (the minimum sum of money) that you **cannot** create. The given coins can have any positive integer value and aren’t necessarily unique (i.e., you can have multiple coins of the same value).
 
--- src/twoNumberSum.js
+++ src/twoNumberSum.js
@@ -1,38 +1,29 @@
-// function twoNumberSum(numbers, targetSum) {
-//     const numMap = {}; 
-    
-//     for (let i = 0; i < numbers.length; i++) {
-//         const complement = targetSum - numbers[i];
-//         if (numMap[complement] !== undefined) {
-           
-//             return [complement, numbers[i]];
-//         }
-       
-//         numMap[numbers[i]] = i;
-//     }
-    
-   
-//     return [];
-// }
-
-// module.exports = { twoNumberSum }
-
 function twoNumberSum(numbers, targetSum) {
 
+    //We check whether the parameter 'numbers' is an array
     if (!Array.isArray(numbers)) {
         throw new TypeError('Input must be an array');
     }
     
+    //If the input is valid, we initialize an empty object to store the numbers from the array 'numbers'
     const numMap = {}; 
     
+    //We iterate though each number in the array, and calculates the complement, i.e. the difference between the targetSum and the current number.
     for (let i = 0; i < numbers.length; i++) {
         const complement = targetSum - numbers[i];
+
+        //We check if the complement exists in the numMap object, if it is different to the current number, and if it a positive number. 
+        //If all the conditions are met, we have found the two numbers that pair up the targetSum,
+        //So we return an array with the complement and the current number.
         if (numMap[complement] !== undefined && numMap[complement] !== i && complement >= 0) {
             return [complement, numbers[i]];
         }
+
+        //if we cannot find a pair, we add the index of the current number to the numMap object.
         numMap[numbers[i]] = i;
     }
     
+    //We return an empty array if the loop completes without finding a valid pair of numbers.
     return [];
 }
 
