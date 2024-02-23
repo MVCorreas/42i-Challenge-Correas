16e0610742463e09bf3882c2a5609f4980d158bb
MVCorreas <correasmv@gmail.com>
2024-02-23 11:21:58 -0300

    Table and Image closer

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("af7629637c145539")
+/******/ 		__webpack_require__.h = () => ("f9ca7380c7fb2a7f")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "997d039e1fcef6bb"; }
+/******/ 		__webpack_require__.h = function() { return "d798220702d2f2d4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
